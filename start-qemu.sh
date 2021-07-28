3af2206062bda4c73dcb7b5e5da5ed64146e2d38
hz2 <writeforever@foxmail.com>
2021-07-28 16:19:56 +0800

    daily update.

--- start-qemu.sh
+++ start-qemu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-arm -M vexpress-a9 -m 512M -kernel zImage2 -nographic -append "root=/dev/mmcblk0 console=ttyAMA0" -sd a9rootfs.ext3
+qemu-system-arm -M vexpress-a9 -m 512M -kernel zImage -nographic -append "root=/dev/mmcblk0 console=ttyAMA0" -sd ./rootfs.ext3
