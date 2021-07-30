5cb28eb5ff38cff5d96314012f2908d1d9b402e8
hz2 <writeforever@foxmail.com>
2021-07-30 18:04:43 +0800

    daily update.

--- start-qemu.sh
+++ start-qemu.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-qemu-system-arm -M vexpress-a9 -m 512M -kernel zImage -nographic -append "root=/dev/mmcblk0 console=ttyAMA0" -sd ./rootfs.ext3
+#MEM_FLAG=-m 512M
+#MACHINE_FLAG="-M vexpress-a9"
+#KERNEL_FLAG="zImage"
+
+#GRAPHIC_FLAG="-nographic"
+#APPEND_FLAG="-append 'root=/dev/mmcblk0 console=ttyAMA0'"
+#ROOTFS_FLAG="-sd rootfs.ext3"
+
+#START_FLAGS=$MACHINE_FLAG $MEM_FLAG $KERNEL_FLAG $GRAPHIC_FLAG $APPEND_FLAG $ROOTFS_FLAG
+
+qemu-system-arm -M vexpress-a9 -m 512M -kernel zImage -append "root=/dev/mmcblk0 console=tty0" -sd ./rootfs.ext3 
